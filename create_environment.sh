1b8458b49325bebb521c626f014a69cdac352562
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-02-20 23:51:46 -0800

    Initail commit.

--- create_environment.sh
+++ create_environment.sh
@@ -8,15 +8,13 @@ MAIN_DIR="submission_reminder_${user_name}"
 
 # Create the main directory
 mkdir -p $MAIN_DIR
+cd $MAIN_DIR || exit
 
 # Create subdirectories 
-mkdir -p $MAIN_DIR/app
-mkdir -p $MAIN_DIR/modules
-mkdir -p $MAIN_DIR/assets
-mkdir -p $MAIN_DIR/config
+mkdir -p app modules assets config 
 
 # Create and populate the reminder.sh file in the directory app
-cat <<_EOF_ > $MAIN_DIR/app/reminder.sh
+cat <<_EOF_ > app/reminder.sh
 #!/bin/bash
 
 # Source environment variables and helper functions
@@ -33,11 +31,11 @@ echo "--------------------------------------------"
 
 check_submissions $submissions_file
 _EOF_
+chmod +x app/reminder.sh
 
 # Create and populate functions.sh file in the moduless directory
-cat <<_EOF_ > $MAIN_DIR/module/function.sh
+cat <<_EOF_ > modules/functions.sh
 #!/bin/bash
-
 # Function to read submissions file and output students who have not submitted
 function check_submissions {
     local submissions_file=$1
@@ -57,23 +55,30 @@ function check_submissions {
     done < <(tail -n +2 "$submissions_file") # Skip the header
 }
 _EOF_
+chmod +x modules/functions.sh
 
 # Create and populate the submission.txt file in the assets directory
-cat <<_EOF_ > $MAIN_DIR/assets/submission.txt
-student, assignment, submission status
+cat <<_EOF_ > assets/submission.txt
+Student, Assignment, Submission Status
 Chinemerem, Shell Navigation, not submitted
 Chiagoziem, Git, submitted
 Divine, Shell Navigation, not submitted
 Anissa, Shell Basics, submitted
 Enoke, Shell Basics, submitted
+Alice, Shell Basics, submitted
+Robert, Git, submitted 
+Raissa, Git, submitted
+Erick, Shell Basics, submitted
+Nadine, Git, submitted
 _EOF_
 
 # Create and populate the config.env file in the config directory 
-cat <<_EOF_ > $MAIN_DIR/config/config.env
+cat <<_EOF_ > config/config.env
 # This is the config file
 ASSIGNMENT="Shell Navigation"
 DAYS_REMAINING=2
 _EOF_
+
 
 # Create the startup.sh file 
 cat <<_EOF_ > $MAIN_DIR/startup.sh
@@ -82,10 +87,7 @@ cat <<_EOF_ > $MAIN_DIR/startup.sh
 # Make run the reminder application
 ./app/reminder.sh
 _EOF_
+chmod +x starup.sh
 
-# Make our scripts executable
-chmod +x $MAIN_DIR/app/reminder.sh
-chmod +x $MAIN_DIR/modules/functions.sh
-chmod +x $MAIN_DIR/startup.sh
-
+# Successful 
 echo "The set up of ENvironment is complete! Run the startup.sh script to start the reminder app."
